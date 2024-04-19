799cedd7105ebcb70437a3122c7bcd115d914cba
Bastien Navarri <navarri.bastien@gmail.com>
2024-04-19 14:32:52 +0200

    add of the csv visualization

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var mouseDownPosition;
 const cellThickness = 0.1;
 
 var game;
-
+var games = [];
 const Type = {
   BORDER: 'border',
   CUBE: 'cube',
@@ -112,13 +112,17 @@ function render() {
 init();
 render();
 
+
 function placeBlock(x, y, z, color){
+    var block = { x: x, y: y, z: z};
+    placedBlocks.push(block);
+
     temp = y
     x = x + 5
     y = z + 5
     z = temp - 1
     var cubeGeometry = new THREE.BoxGeometry(cellSize, cellSize, cellSize);
-    var cubeMaterial = new THREE.MeshBasicMaterial({ color: color});
+    var cubeMaterial = new THREE.MeshBasicMaterial({ color: Color[color.toUpperCase()]});
     var cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cubeMesh.position.set(
         (x - (gridSize - 1) / 2) * (cellSize + borderThickness),
@@ -138,7 +142,15 @@ function placeBlock(x, y, z, color){
 }
 
 
-const breakBlock = (x, y, z) => {
+function breakBlock (x, y, z){
+    for (var i = 0; i < placedBlocks.length; i++) {
+        var block = placedBlocks[i];
+        if (x === block.x && y === block.y && z === block.z) {
+            placedBlocks.splice(i, 1);
+            break;
+        }
+    }
+
     temp = y
     x = x + 5
     y = z + 5
@@ -160,37 +172,68 @@ const addChat = (message) => {
 };
 
 var changes = [];
-document.getElementById('fileInput').addEventListener('change',function() {
-    var file = this.files[0]
+document.getElementById('fileInput').addEventListener('change', function () {
+    var file = this.files[0];
     var reader = new FileReader();
-    reader.onload = function(event) {
-        game = JSON.parse(event.target.result).WorldStates;
 
-        for (let i = 1; i < game.length; i++) {
-            chat = game[i].ChatHistory.filter(item => !game[i-1].ChatHistory.includes(item))
-            if(chat != ""){
-                changes.push({ func: addChat, values: [chat[0]]})
-            } else {
-                blocks = game[i].BlocksInGrid.filter(item => {
-                    return !game[i-1].BlocksInGrid.some(prevItem => {
-                        return prevItem.X === item.X && prevItem.Y === item.Y && prevItem.Z === item.Z;
-                    });
-                })
-                if(blocks.length == 1){
-                    changes.push({ func: placeBlock, values: [blocks[0].X, blocks[0].Y, blocks[0].Z, blocks[0].Colour]})
-                } else {
-                    blocks = game[i - 1].BlocksInGrid.filter(item => {
-                        return !game[i].BlocksInGrid.some(prevItem => {
-                            return prevItem.X === item.X && prevItem.Y === item.Y && prevItem.Z === item.Z;
+    reader.onload = function (event) {
+        if (file.name.endsWith('.json')) {
+            try {
+                console.log(event);
+                game = JSON.parse(event.target.result).WorldStates;
+
+                for (let i = 1; i < game.length; i++) {
+                    chat = game[i].ChatHistory.filter(item => !game[i - 1].ChatHistory.includes(item));
+                    if (chat != "") {
+                        changes.push({ func: addChat, values: [chat[0]] });
+                    } else {
+                        blocks = game[i].BlocksInGrid.filter(item => {
+                            return !game[i - 1].BlocksInGrid.some(prevItem => {
+                                return prevItem.X === item.X && prevItem.Y === item.Y && prevItem.Z === item.Z;
+                            });
                         });
-                    })
-                    changes.push({ func: breakBlock, values: [blocks[0].X, blocks[0].Y, blocks[0].Z]})
+                        if (blocks.length == 1) {
+                            changes.push({ func: placeBlock, values: [blocks[0].X, blocks[0].Y, blocks[0].Z, blocks[0].Colour] });
+                        } else {
+                            blocks = game[i - 1].BlocksInGrid.filter(item => {
+                                return !game[i].BlocksInGrid.some(prevItem => {
+                                    return prevItem.X === item.X && prevItem.Y === item.Y && prevItem.Z === item.Z;
+                                });
+                            });
+                            changes.push({ func: breakBlock, values: [blocks[0].X, blocks[0].Y, blocks[0].Z] });
+                        }
+                    }
                 }
+                document.getElementById('hider').style.display = "none";
+            } catch (error) {
+                console.error("Error parsing JSON file:", error);
             }
+        } else if (file.name.endsWith('.csv')) {
+            document.getElementById('hider').style.display = "none";
+            document.querySelector('header').style.display = "flex";
+            const regex = /^"(.+?)","(.*?)"$/mgs;
+
+            let match;
+            while ((match = regex.exec(event.target.result)) !== null) {
+                games.push({
+                    "dial_with_actions": match[1],
+                    "pred_seq": match[2].split("\n").map(e => e.trim()).map(text => (
+                        text.startsWith("place")
+                        ? ({ func: placeBlock, values: [parseInt(text.split(" ")[2]), parseInt(text.split(" ")[3]), parseInt(text.split(" ")[4]), text.split(" ")[1]] })
+                        : ({func: breakBlock, values: [parseInt(text.split(" ")[1]), parseInt(text.split(" ")[2]), parseInt(text.split(" ")[3])]}) 
+                    ))
+                })
+            }
+            historyChatElement.textContent = games[0]["dial_with_actions"]
+            changes = games[0]["pred_seq"]
+        } else {
+            console.error("Unsupported file format. Please upload a JSON or CSV file.");
         }
-        document.getElementById('hider').style.display = "none"
     };
-    reader.readAsText(file);
+
+    if (file) {
+        reader.readAsText(file);
+    }
 });
 
 currentStatus = -1
@@ -200,8 +243,50 @@ function play(i){
 
 const nextElement = document.querySelector('#side > button');
 nextElement.addEventListener("click", (event) => {
+    console.log(changes, currentStatus)
     if(currentStatus < changes.length - 1){
         currentStatus += 1
         play(currentStatus)
+    }
+})
+
+currentGame = 0
+const spanHeader = document.querySelector('header > span')
+const leftButton = document.querySelector('header > .left')
+leftButton.addEventListener("click", (event) => {
+    console.log("left")
+    currentGame -= 1
+    spanHeader.textContent = `Game ${currentGame + 1}`
+    spanHeader.setAttribute("number", currentGame + 1)
+    
+    for (let index = placedBlocks.length - 1; index >= 0 ; index--) {
+        breakBlock(placedBlocks[index].x,placedBlocks[index].y,placedBlocks[index].z)
+    }
+    
+    currentStatus = -1
+
+    historyChatElement.textContent = games[currentGame]["dial_with_actions"]
+    changes = games[currentGame]["pred_seq"]
+    rightButton.style.display = "block";
+})
+
+const rightButton = document.querySelector('header > .right')
+rightButton.addEventListener("click", (event) => {
+    console.log("right")
+    currentGame += 1
+    spanHeader.textContent = `Game ${currentGame + 1}`
+    spanHeader.setAttribute("number", currentGame + 1)
+    
+    for (let index = placedBlocks.length - 1; index >= 0 ; index--) {
+        breakBlock(placedBlocks[index].x,placedBlocks[index].y,placedBlocks[index].z)
+    }
+    
+    currentStatus = -1
+
+    historyChatElement.textContent = games[currentGame]["dial_with_actions"]
+    changes = games[currentGame]["pred_seq"]
+
+    if(currentGame + 1 == games.length){
+        rightButton.style.display = "none";
     }
 })
